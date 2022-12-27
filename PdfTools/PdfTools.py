aa91f450a135f35323c780488d8f7e236efdd0d8
suryaya <tahasoomro888@gmail.com>
2022-12-27 14:05:19 +1100

    EOD updates

--- PdfTools/PdfTools.py
+++ PdfTools/PdfTools.py
@@ -1,5 +1,4 @@
 import os
-
 import pdfplumber
 import re
 
@@ -17,24 +16,18 @@ def detect_input_pdfs(self, input_path):
     return input_pdfs
 
 
-class RequirementsScraper:
+class PdfSettings:
+    """ This class provides PDF settings applicable to all pdf manipulation functions and methods."""
 
-    def __init__(self, input_path, output_path, table_settings, pdf_schema):
-        self.input_path = input_path
-        self.output_path = output_path
-        self.table_settings = table_settings  # These should be class variables not functions. Restructure your code
-        self.pdf_schema = pdf_schema
-
-        # Class-wide settings
-        table_resolution = 100
+    def __init__(self):
+        pass
 
     class TableSettings:
-        # Need to think about best method to
         """ This class stores detection parameters for PDFPlumber. Currently only supports TfNSW table preset (should be
         good enough for most cases """
 
         def __init__(self):
-            self.table_settings = self.settings()
+            self.table_parameters = self.settings()
 
         @classmethod
         def settings(cls, preset='TfNSW'):
@@ -69,25 +62,14 @@ class RequirementsScraper:
             except UnboundLocalError:
                 print(f"Error! No table preset \"{preset}\" exists.")
 
-    class PdfSettings:
+    class PageMargins:
 
         """ Set parameters related to page / text layout such as regexes for finding requirements and
         setting page margins"""
 
-        def __init__(self):
+        def __init__(self, page):
 
-            self.chapter_headings, self.requirements = self.search_patterns()
-            self.page_margins = self.page_margins()
-
-        @classmethod
-        def search_patterns(cls, preset='TfNSW'):
-            if preset == 'TfNSW':
-                chapter_headings = r"(\n\s*\d[.]?\d?[.]?\d?\s+[A-Z].*)"
-                requirements = r"(?sm)^([(]\w{0,4}[)]\s)(.*?)(?=^[(]\w{0,4}[)]\s)"
-            try:
-                return chapter_headings, requirements
-            except UnboundLocalError:
-                print(f"Error! No pdf page preset {preset} exists.")
+            self.page_margins = self.page_margins(page)
 
         @classmethod
         def page_margins(cls, page, preset='TfNSW'):
@@ -102,54 +84,75 @@ class RequirementsScraper:
             except UnboundLocalError:
                 print(f"Error! No pdf page preset {preset} exists.")
 
-    def pdf_schema_helper(self, page):
 
-        """This function provides specific information about the requirement brief's format,
-        such as the regexes for chapter headings and individual requirements, as well as
-        the bounding box for page to exclude headers. pdfplumber page is an input so margins are returned."""
+class RequirementsScraper:
 
-        if self.pdfSchema == 'TfNSW':
-            chapter_headings = r"(\n\s*\d[.]?\d?[.]?\d?\s+[A-Z].*)"
-            requirements = r"(?sm)^([(]\w{0,4}[)]\s)(.*?)(?=^[(]\w{0,4}[)]\s)"
-            page_margins = (0, 50, page.width, page.height - 70)
+    def __init__(self, input_path, output_path):
+        self.input_path = input_path
+        self.output_path = output_path
 
-        try:
-            return chapter_headings, requirements, page_margins,
+    class SearchPatterns:
+        def __init__(self, chapter_headings, requirements):
+            self.chapter_headings, self.requirements = self.search_patterns()
 
-        except NameError:
-            print("Invalid PDF schema selected!")
+        @classmethod
+        def search_patterns(cls, preset='TfNSW'):
+            if preset == 'TfNSW':
+                chapter_headings = r"(\n\s*\d[.]?\d?[.]?\d?\s+[A-Z].*)"
+                requirements = r"(?sm)^([(]\w{0,4}[)]\s)(.*?)(?=^[(]\w{0,4}[)]\s)"
+            try:
+                return chapter_headings, requirements
+            except UnboundLocalError:
+                print(f"Error! No pdf page preset {preset} exists.")
 
     @staticmethod
-    def scrape_pdfs(self, extract_tables=True):
+    def scrape_pdfs(self, chapter_headings, requirements, table_settings, extract_tables=True, table_resolution=100):
 
         """Main function to convert document requirements to a dataframe."""
         input_pdfs = self.detect_input_pdfs(self.input_path)
 
-        pdfreader = pdfplumber.open(self.input_path)
-        pages = pdfreader.pages
-
         # Indexes
         table_index = 0
 
-        for page in pages:
+        # Folder for saving images:
 
-            chapter_headings, requirements, page_margins = self.pdf_schema_helper(page)
+        if extract_tables:
+            temp_folder = self.output_path()
+            if not os.path.exists(table_filepath):
+                os.makedirs(table_filepath)
 
-            # Crop header, extract text, locate tables to extract later/ remove text
-            page = page.crop(page_margins)
-            page_text = page.extract_text()
-            tables = page.find_tables()
-            tables_bbox = []
+        for input_pdf in input_pdfs:
 
-            for table in tables:
-                table_index += 1
-                tables_bbox.append(table.bbox)
-                table_filename = f"TABLE {table_index}.png"
-                temp_folder = self.output_path()
-                table_filepath = os.path.join(self.input_path, "scrape_temp_folder")
-                if not os.path.exists(table_filepath):
-                    os.makedirs(table_filepath)
-                page.crop(table.bbox).to_image()
-            if extract_tables:
-                pass
-                # Find tables
+            pdfreader = pdfplumber.open(self.input_path)
+            pages = pdfreader.pages
+
+            for page in pages:
+
+                page_margins = PdfSettings.PageMargins.page_margins(page)
+
+                # Crop header, extract text, locate tables to extract later/ remove text
+                page = page.crop(page_margins)
+                page_text = page.extract_text()
+                tables = page.find_tables()
+                tables_bbox = []
+
+                for table in tables:
+                    table_index += 1
+                    tables_bbox.append(table.bbox)
+
+
+
+
+
+                for table in tables:
+                    # Loop over each table found, add bounding box to list. Create dummy folder for tables
+                    table_index += 1
+                    tables_bbox.append(table.bbox)
+                    table_filename = f"TABLE {table_index}.png"
+
+                    table_filepath = os.path.join(self.input_path, "scrape_temp_folder")
+                    page.crop(table.bbox).to_image()
+
+                if extract_tables:
+                    pass
+                    # Find tables
